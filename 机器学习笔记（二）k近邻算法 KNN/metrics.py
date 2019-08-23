35e2f16daaf85c7f74061b8906ef8690a395c991
CharlesKins <CharlesKins@163.com>
2019-08-23 18:19:40 +0800

    添加封装好的函数

--- 机器学习笔记（二）k近邻算法 KNN/metrics.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def accuracy_score(y_true, y_predict):
-
-    assert y_true.shape[0] == y_predict.shape[0], \
-    "the size of y_true must be equal to the size of y_predict"
-
-    return  sum(y_predict == y_true) / len(y_true)
